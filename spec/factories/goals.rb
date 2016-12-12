face719664d61657fb4612b39beb4875ec4fcaa5
PrestonPhelan <preston.phelan@gmail.com>
2016-12-12 14:45:07 -0800

    Added specs for Goals feature

--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -1,5 +1,10 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :goal do
-    
+    goal_name { Faker::Name.name }
+    user_id 1
+    completed false
+    priv false
   end
 end
---
+++ spec/features/goal_spec.rb
@@ -0,0 +1,150 @@
+require 'spec_helper'
+require 'rails_helper'
+
+before(:all) do
+  User.create(username: "sadclown", password: "sadness")
+  User.create(username: "happyclown", password: "sohappy")
+  login(User.find(1))
+end
+
+feature "adding goals" do
+  before(:each) { visit user_url }
+
+  scenario "user's page has an add goals button" do
+    expect(page).to have_content("Add Goal")
+  end
+
+  feature "add goal page" do
+    before(:each) { click_on "Add Goal" }
+
+    scenario "has an add goals page" do
+      expect(page).to have_content("Add Goal")
+    end
+
+    scenario "has fields for all columns" do
+      expect(page).to have_content("Goal")
+    end
+
+    scenario "new goal appears on the user's homepage" do
+      fill_in 'Goal', with: 'Finish Capybara spec!'
+      expect(page).to have_content("Finish Capybara spec!")
+    end
+  end
+end
+
+feature "viewing own goals" do
+  FactoryGirl.create(:goal, goal_name: "First goal", user_id: 1)
+  FactoryGirl.create(:goal, goal_name: "Second goal", user_id: 1)
+  FactoryGirl.create(:goal, goal_name: "Secret goal", private: true, user_id: 1)
+  before(:each) { visit user_url(1) }
+
+  scenario "user's home page lists all goals" do
+    expect(page).to have_content("First goal")
+    expect(page).to have_content("Second goal")
+  end
+
+  scenario "user's home page lists their own private goals" do
+    expect(page).to have_content("Secret goal")
+  end
+end
+
+feature "viewing other user's goals" do
+  FactoryGirl.create(:goal, goal_name: "First goal", user_id: 2)
+  FactoryGirl.create(:goal, goal_name: "Second goal", user_id: 2)
+  FactoryGirl.create(:goal, goal_name: "Secret goal", private: true, user_id: 2)
+  before(:each) { visit user_url(2) }
+
+  scenario "other user's page shows their public goals" do
+    expect(page).to have_content("First goal")
+    expect(page).to have_content("Second goal")
+  end
+
+  scenario "other user's page does not show their private goals" do
+    expect(page).to_not have_content("Secret goal")
+  end
+end
+
+feature "updating goals" do
+  scenario "user's page has links to update goal" do
+    FactoryGirl.create(:goal, goal_name: "First goal", user_id: 1)
+    visit user_url(1)
+    expect(page).to have_content("Update Goal")
+  end
+
+  scenario "button not available to other users" do
+    FactoryGirl.create(:goal, goal_name: "First goal", user_id: 2)
+    visit user_url(2)
+    expect(page).to_not have_content("Update Goal")
+  end
+
+  feature "update goal page" do
+    before(:each) do
+      FactoryGirl.create(:goal, goal_name: "First goal", user_id: 1)
+      visit user_url(1)
+      click_on "Update Goal"
+    end
+
+    scenario "has an update goal page" do
+      expect(page).to have_content("Update Goal")
+    end
+
+    scenario "pre-fills form with goal values" do
+      expect(page).to have_content("First Goal")
+    end
+
+    scenatio "after update, redirects to user's homepage" do
+      click_on "Update"
+      expect(response).to redirect_to(user_url(1))
+    end
+  end
+end
+
+feature "completing goals" do
+  scenario "user's page has links to complete goal" do
+    FactoryGirl.create(:goal, user_id: 1)
+    visit user_url(1)
+    expect(page).to have_content("Complete!")
+  end
+
+  scenario "button not available to other users" do
+    FactoryGirl.create(:goal, user_id: 2)
+    visit user_url(2)
+    expect(page).to_not have_content("Complete!")
+  end
+
+  scenario "completed goals show 'Completed'" do
+    FactoryGirl.create(:goal, user_id: 1)
+    visit user_url(1)
+    click_on "Complete!"
+    expect(page).to have_content("Completed")
+    expect(Goal.find(1).completed).to be true
+  end
+end
+
+feature "deleting goals" do
+  before(:all) { FactoryGirl.create(:goal, goal_name: "First Goal", user_id: 1) }
+
+  scenario "user's page has links to delete goal" do
+    visit user_url(1)
+    expect(page).to have_content("Delete Goal")
+  end
+
+  scenario "button not available to other users" do
+    FactoryGirl.create(:goal, user_id: 2)
+    visit user_url(2)
+    expect(page).to_not have_content("Delete Goal")
+  end
+
+  scenario "after delete, no longer appears on user's page" do
+    visit user_url(1)
+    expect(page).to have_content("First Goal")
+    click_on "Delete Goal"
+    expect(page).to_not have_content("First Goal")
+  end
+
+  scenario "shows 'Goal deleted!' after deleting a goal" do
+    visit user_url(1)
+    click_on "Delete Goal"
+    expect(page).to have_content("Goal deleted!")
+  end
+end
